96b9102644586adcb0f35ca2b86c6278a436fd24
Alistair Kane <alistairkane92@hotmail.co.uk>
2019-01-14 19:40:58 +0000

    closures 2

--- closure.js
+++ closure.js
@@ -13,3 +13,15 @@ console.log(wrap2());
 // different calls don't trample on each local variable
 // being able to reference a specific instance of local variable inside function is called 'closure'
 // inner function is 'a closure' because it makes local variable private (i.e closes over)
+
+function multiplier(factor){
+  return function(number){
+    return number * factor;
+  }
+}
+
+var twice = multiplier(2);
+console.log(twice);
+console.log(twice(5));
+
+// above function "freezes" local variable "factor" on initial call of multiplier function and "unfreezes" the variable for the call of the inner function. 
